045efb5f6af1d8977c1798cf631b75cbeecaea56
Jacob Kofod <jacob_kofod@yahoo.com>
2023-09-25 15:57:29 +0200

    Package script fix

--- package.ps1
+++ package.ps1
@@ -1,6 +1,6 @@
 $date = Get-Date
 $version = $date.ToString("yyyy-dd-M--HH-mm-ss")
-$filename = "S3JsonBot-" + $version + ".zip"
+$filename = "S3JsonCalculator-" + $version + ".zip"
 cd .\S3JsonCalculator\src\S3JsonCalculator
 dotnet lambda package ..\..\..\Packages\$filename --configuration Release -frun dotnet6 -farch arm64
 cd ..\..\..
